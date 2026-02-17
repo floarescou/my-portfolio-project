657069fd16d323d43885e9e9b10c80e84d68f890
Franka <floarescou@yandex.ru>
2026-02-17 21:35:20 +0300

    scroll into view js for navigation

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-// alert("Hello, JavaScript");
+// плавная прокрутка к нужной секции
+const navBtns = document.getElementsByClassName("nav__item");
+
+const sections = [
+    document.getElementById("about"),
+    document.getElementById("skills"),
+    document.getElementById("portfolio")
+];
+
+for (let i = 0; i < sections.length; i++) {
+    navBtns[i].addEventListener('click', function (event) {
+        sections[i].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    })
+}
+
+// .. плавная прокрутка к нужной секции
+
+
